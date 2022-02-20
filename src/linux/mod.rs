5106c6fe25477b2b81391b4df4f18791e2950a1d
mich4ld <corollid@gmail.com>
2022-02-20 10:57:36 +0100

    add memory info

--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -1,5 +1,4 @@
 use std::fs;
-
 pub struct Linux {}
 
 impl Linux {
@@ -34,6 +33,53 @@ impl Linux {
         Some(os_name)
     }
 
-    pub fn memory(&self) {
+    pub fn memory(&self) -> Option<Memory> {
+        let meminfo = fs::read_to_string("/proc/meminfo").ok()?;
+        let mut memory = Memory::default();
+
+        for line in meminfo.lines() {
+            if line.is_empty() {
+                continue;
+            }
+
+            let entries = line.split_whitespace().collect::<Vec<&str>>();
+            if entries.len() < 2 {
+                continue;
+            }
+            
+            match entries[0] {
+                "MemTotal:" => {
+                    memory.total = entries[1].parse().ok()?;
+                }
+                "MemFree:" => {
+                    memory.free = entries[1].parse().ok()?;
+                }
+                "MemAvailable:" => {
+                    memory.available = entries[1].parse().ok()?;
+                }
+                _ => {
+                    continue;
+                }
+            }
+        }
+
+        Some(memory)
+    }
+}
+
+#[derive(Debug)]
+pub struct Memory {
+    pub total: usize,
+    pub available: usize,
+    pub free: usize,
+}
+
+impl Default for Memory {
+    fn default() -> Self {
+        Memory {
+            total: 0,
+            available: 0,
+            free: 0,
+        }  
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use fetch_os::linux;
 
 fn main() {
     let os = linux::Linux {};
-    os.memory();
+    let memory = os.memory().unwrap();
     let os = os.name().unwrap();
     println!("{}", os);
+    println!("{}mb / {}mb", (memory.total - memory.available) / 1024, memory.total / 1024);
 }
