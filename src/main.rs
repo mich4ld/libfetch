8d39a91b077d95fbb2f126c7bc7afbc68c3c7776
mich4ld <corollid@gmail.com>
2022-02-21 11:28:18 +0100

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
+use fetch_os::{linux, platform::Platform};
 
+fn main() {
+    let os = linux::Linux {};
+
+    let os_name = os.name();
+    let kernel = os.kernel();
+
+    println!("os ~> {}", os_name.unwrap_or(String::from("unknown")));
+    println!("kernel ~> {}", kernel.unwrap_or(String::from("unknown")));
 }
